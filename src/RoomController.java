bdc2c579c433e6c89bd6d639e0fecaaa6a6b4cb0
yoo <jungjungyoo@gmail.com>
2022-05-04 10:24:40 +0900

    .

--- src/RoomController.java
+++ src/RoomController.java
@@ -240,7 +240,7 @@ public class RoomController implements Initializable {
         separator = File.separator;
         if(separator.equals("\\"))
             separator += separator;
-        dirPath = getClass().getResource("").getPath() + "file" + separator;
+        dirPath = "file" + separator;
     }
 
     public void setPrimaryStage(Stage primaryStage) {
